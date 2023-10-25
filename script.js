c06fef642e39f79e4f279fbf13839a65200d804f
nharjes <nilsdevelops@outlook.de>
2023-10-25 12:42:08 +0200

    added EventListener

--- script.js
+++ script.js
@@ -1,70 +1,86 @@
 // ----------in this section we handle the unsplash API and get the background image----------
 
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=art")
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        document.body.style.backgroundImage = `url(${data.urls.full})`
-		document.getElementById("author").textContent = `By: ${data.user.name}`
-        document.getElementById("author").addEventListener("click", (e)=> window.open(`${data.user.links.html}`))
-    })
-    .catch(err => {
-        // ----------here we use a default background image/author----------
-        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
-)`
-		document.getElementById("author").textContent = `By: Dodi Achmad`
-    })
+fetch(
+  "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=art"
+)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    document.body.style.backgroundImage = `url(${data.urls.full})`;
+    document.getElementById("author").textContent = `By: ${data.user.name}`;
+    document
+      .getElementById("author")
+      .addEventListener("click", (e) => window.open(`${data.user.links.html}`));
+  })
+  .catch((err) => {
+    // ----------here we use a default background image/author----------
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
+)`;
+    document.getElementById("author").textContent = `By: Dodi Achmad`;
+  });
 
 // ----------in this section we handle the Coingecko API and get the price data----------
 
-    fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
-    .then(res => {
-        if (!res.ok) {
-            throw Error("Something went wrong")
-        }
-        return res.json()
-    })
-    .then(data => {
-        document.getElementById("crypto-top").innerHTML = `
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+  .then((res) => {
+    if (!res.ok) {
+      throw Error("Something went wrong");
+    }
+    return res.json();
+  })
+  .then((data) => {
+    document.getElementById("crypto-top").innerHTML = `
             <img src=${data.image.small} />
             <span>${data.name}</span>
-        `
-        document.getElementById("crypto").innerHTML += `
+        `;
+
+    document.getElementById("crypto").innerHTML += `
             <p>➡️: $${data.market_data.current_price.usd}</p>
             <p>↗️: $${data.market_data.high_24h.usd}</p>
             <p>↘️: $${data.market_data.low_24h.usd}</p>
-        `
-    })
-    .catch(err => console.error(err))
+        `;
+  })
+  .catch((err) => console.error(err));
+document
+  .getElementById("crypto")
+  .addEventListener("click", (e) => window.open(`https://www.coingecko.com/`));
+
+//----------- in this section we handle the Date and get the current Time
 
 function getCurrentTime() {
-    const date = new Date()
-    document.getElementById("time").textContent = date.toLocaleTimeString("en-us", {timeStyle: "short"})
+  const date = new Date();
+  document.getElementById("time").textContent = date.toLocaleTimeString(
+    "en-us",
+    { timeStyle: "short" }
+  );
 }
 
-setInterval(getCurrentTime, 1000)
+setInterval(getCurrentTime, 1000);
 
-//---------- in this section we handle the Weather API and apply the user coordinates---------- 
+//---------- in this section we handle the Weather API and apply the user coordinates----------
 
-navigator.geolocation.getCurrentPosition(position => {
-    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
-        .then(res => {
-            if (!res.ok) {
-                throw Error("Weather data not available")
-            }
-            return res.json()
-        })
-        .then(data => {
-            const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-            document.getElementById("weather").innerHTML = `
+navigator.geolocation.getCurrentPosition((position) => {
+  fetch(
+    `https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`
+  )
+    .then((res) => {
+      if (!res.ok) {
+        throw Error("Weather data not available");
+      }
+      return res.json();
+    })
+    .then((data) => {
+      const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+      document.getElementById("weather").innerHTML = `
                 <img src=${iconUrl} />
                 <p class="weather-temp">${Math.round(data.main.temp)}º</p>
                 <p class="weather-city" id="city-name">${data.name}</p>
-            `
-            document.getElementById("city-name").addEventListener("click", (e)=> window.open(`https://www.google.com/search?q=${data.name}`))
-        })
-        .catch(err => console.error(err))
+            `;
+      document
+        .getElementById("city-name")
+        .addEventListener("click", (e) =>
+          window.open(`https://www.google.com/search?q=${data.name}`)
+        );
+    })
+    .catch((err) => console.error(err));
 });
-
-
-
